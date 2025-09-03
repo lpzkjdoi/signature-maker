5db462660dec08b10ba78e97b41ce2a6b6328ca7
lpzkjdoi <lpzkjdoi@icloud.com>
2025-09-03 18:22:32 +0200

    Person class updated with getters added

--- entities/person.py
+++ entities/person.py
@@ -9,3 +9,12 @@ class Person:
     job: str
     tel1: str
     tel2: str
+
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+
+    def get_tel(self):
+        if self.tel2:
+            return self.tel1 + " - " + self.tel2
+        else:
+            return self.tel1
